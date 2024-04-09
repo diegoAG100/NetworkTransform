99cf79707aff5f57767b089b4c03dbd589f1fcbd
diegoAG100 <diegoamenedogonzalez01@gmail.com>
2024-04-09 21:14:24 +0200

    salto realizado

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -7,6 +5,14 @@ using UnityEngine;
 public class PlayerMovement : NetworkBehaviour
 {
     public float velocity = 6;
+    public float jumpforce = 6;
+    private Vector3 movement = new Vector3();
+
+    private Rigidbody rb;
+
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -16,23 +22,38 @@ public class PlayerMovement : NetworkBehaviour
         }
     }
 
+    void Update(){
+        if(IsOwner){
+            Jump();
+        }
+    }
+
     private void Move(){
-        Vector3 movement = new Vector3();
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         movement.x = horizontal;
         movement.z = vertical;
 
-        movement=movement*velocity*Time.fixedDeltaTime;
+        movement=movement.normalized*velocity*Time.fixedDeltaTime;
 
-        transform.position +=movement;
         SubmitPositionRequestServerRpc(movement);
     }
 
     [Rpc(SendTo.Server)]
     void SubmitPositionRequestServerRpc(Vector3 movement){
         transform.position +=movement;
+    }
+
+    private void Jump(){
+        if(Input.GetButtonDown("Jump")&&transform.position.y<1.2f){
+            SubmitJumpRequestServerRpc();
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    void SubmitJumpRequestServerRpc(){
+        rb.AddForce(Vector3.up*jumpforce,ForceMode.Impulse);
     }
 
 }
